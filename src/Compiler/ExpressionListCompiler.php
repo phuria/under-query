071b246f70e2e8379e16ea81a9144de9ea220f39
Beniamin Jonatan Šimko <phuria@o2.pl>
2016-08-21 14:33:39 +0200

    added php file header

--- src/Compiler/ExpressionListCompiler.php
+++ src/Compiler/ExpressionListCompiler.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of Phuria SQL Builder package.
+ *
+ * Copyright (c) 2016 Beniamin Jonatan Šimko
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Phuria\QueryBuilder\Compiler;
 
 use Phuria\QueryBuilder\Expression\ExpressionInterface;
