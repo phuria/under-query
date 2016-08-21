6f70d100e4079ab56f8c4803e40de0f1ce2d4a0b
Beniamin Jonatan Šimko <phuria@o2.pl>
2016-08-21 22:53:25 +0200

    test fixed

--- tests/Unit/TableRecognizerTest.php
+++ tests/Unit/TableRecognizerTest.php
@@ -30,9 +30,6 @@ class TableRecognizerTest extends \PHPUnit_Framework_TestCase
         $type = $recognizer->recognizeType(function (ExampleTable $table) { });
         static::assertSame(TableRecognizer::TYPE_CLOSURE, $type);
 
-        $type = $recognizer->recognizeType('example.route.to.other.table');
-        static::assertSame(TableRecognizer::TYPE_ROUTE, $type);
-
         $type = $recognizer->recognizeType(ExampleTable::class);
         static::assertSame(TableRecognizer::TYPE_CLASS_NAME, $type);
 
